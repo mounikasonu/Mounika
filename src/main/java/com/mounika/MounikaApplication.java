a99eed570497ea79b4b497d924b99af255f6dd8d
mounika.shivannagari <mounika.shivannagari@thrymr.net>
2019-11-09 17:26:31 +0530

    my basic commit

---
+++ src/main/java/com/mounika/MounikaApplication.java
@@ -0,0 +1,13 @@
+package com.mounika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MounikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MounikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mounika/MounikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mounika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MounikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
